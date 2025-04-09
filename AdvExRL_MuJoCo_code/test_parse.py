aaa152b36049b88c3d48b3df041369ba6b872ba4
NWhitener <nathanpwhitener@gmail.com>
2025-04-09 11:47:28 -0400

    clean-up

--- AdvExRL_MuJoCo_code/test_parse.py
+++ AdvExRL_MuJoCo_code/test_parse.py
@@ -6,17 +6,16 @@ import math
 class parser(): 
     def __init__(self, observation, action):
         
-        #Parse the left/right action 
+        #Parse the Action
         self.action_left_right = action[0]
-        #Parse the up/down action
         self.action_up_down = action[1]
-        
         self.action_left_right_word = None
         self.action_up_down_word = None
-
         self.magnitude = None
-
         self.action_sentence = None
+
+        #Parse the OBS 
+        
         
     def parse_action_to_directions(self): 
         #See if the action is left or right
@@ -37,22 +36,22 @@ class parser():
         self.magnitude = round(math.sqrt((self.action_left_right**2) + (self.action_up_down **2)),2)
 
     def create_sentence_action(self): 
-        if self.magnitude > 1: 
-            direction = 'Strong'
-        elif 0.4 < self.magnitude < 1: 
+        self.parse_action_to_directions()
+        self.calc_magnitude()
+        if self.magnitude < 0.4: 
+            direction = 'Weak'
+        elif 0.4 < self.magnitude < 0.92: 
             direction = 'Moderate'
         else: 
-            direction = 'Weak'
+            direction = 'Strong'
 
-        self.action_sentence = f"The agent moved {self.action_left_right_word} and {self.action_up_down_word} {direction}ly"
+        self.action_sentence = f"The agent moved {self.action_left_right_word} and {self.action_up_down_word} {direction}ly with magnitude {self.magnitude}"
 
 
 if __name__ == '__main__': 
     action = [ 0.36657423,-0.535817]
     obs = [0,0,0,0]
     parser = parser(obs, action)
-    parser.parse_action_to_directions()
-    parser.calc_magnitude()
     parser.create_sentence_action()
     print(parser.action_sentence)
     print(f"The agent took the directions {parser.action_left_right_word}, {parser.action_up_down_word} with magnitude {parser.magnitude}")
